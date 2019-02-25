201939333ea63c8e67f0b250e1d16d5be44fff27
sahilbhosale63 <sahilbhosale63@live.com>
2019-02-25 15:04:59 +0530

    minor changes to main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       home:new LoginPage(), 
       theme:new ThemeData(primarySwatch:Colors.blue), 
     ); 
